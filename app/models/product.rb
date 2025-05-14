3b9237a9093d12f9e9791c1dbd4d9a62bc629eb6
pittalaabhishek <pittalaabhishek999@gmail.com>
2025-05-14 17:43:57 +0530

    Fix cacading delete for the products table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,23 @@
 class Product < ActiveRecord::Base
-  has_many :product_specs, dependent: :destroy
+  has_many :product_specs
 
   validates :name, presence: true
+  # before_destroy :delete_specs
+
+  def destroy
+    # Start a transaction
+    Product.transaction do
+      # First delete all associated product_specs
+      ProductSpec.delete_all(product_id: id)
+      
+      # Then delete the product itself
+      super
+    end
+  end
+
+  # private
+  
+  # def delete_specs
+  #   ProductSpec.delete_all(product_id: id)
+  # end
 end
--- app/models/product_spec.rb
+++ app/models/product_spec.rb
@@ -1,6 +1,5 @@
 class ProductSpec < ActiveRecord::Base
   belongs_to :product
-  
   validates :key, presence: true
   validates :value, presence: true
 end
